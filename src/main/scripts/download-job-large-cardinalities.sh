87a57e0a307a8ba9ef15d5f7a571466efff49c9f
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2025-08-04 11:45:20 +0200

    Update link to download JOBLarge cardinalities

--- src/main/scripts/download-job-large-cardinalities.sh
+++ src/main/scripts/download-job-large-cardinalities.sh
@@ -4,6 +4,6 @@ PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
 CARDINALITIES_PATH="$PROJECT_ROOT/benchmarks/job-large/cardinalities"
 
 mkdir -p "$CARDINALITIES_PATH"
-wget "https://drive.usercontent.google.com/download?id=1wcs_613P-aAWLoEIQJuo3xOEmbk4PcC-&export=download&authuser=1&confirm=t&uuid=8c64c2a8-811f-4b15-919f-73003b8c8900&at=AN8xHoqvgWDPun0Uv3t3gwkMOEUS:1754060710535" -O "$PROJECT_ROOT/job-large-cardinalities.zip"
+wget "https://drive.usercontent.google.com/download?id=19fUchVtL6l_rAlzBG3mIJVYl_W9lYP5l&export=download&confirm=t&uuid=a878691d-d6f4-4752-a29f-c409b904ade2" -O "$PROJECT_ROOT/job-large-cardinalities.zip"
 tar -xzf job-large-cardinalities.zip -C "$CARDINALITIES_PATH"
 rm "$PROJECT_ROOT/job-large-cardinalities.zip"
