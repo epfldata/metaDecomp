3fb137be3dbf49b395fb9fe91c31768328d25f0e
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2025-10-16 17:33:05 +0200

    Update download link

--- src/main/scripts/setup.sh
+++ src/main/scripts/setup.sh
@@ -2,7 +2,7 @@
 
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
 
-wget "https://drive.usercontent.google.com/download?id=16PE9TIeTvDZQQWR3eDxgyfZzFmeVZzgA&export=download&authuser=1&confirm=t&uuid=2218cb4c-35cd-4ebc-9835-6ebe5937b80b&at=AKSUxGN-N7cZUXIYiR0tvoXWOA-e:1760623677259" -O "$PROJECT_ROOT/benchmarks.zip"
+wget "https://drive.usercontent.google.com/download?id=16PE9TIeTvDZQQWR3eDxgyfZzFmeVZzgA&export=download&authuser=1&confirm=t&uuid=9b5fb26c-5c4b-4015-8953-7e565f1a54e3&at=AKSUxGMsrKSSDlYVNa2OtlhDFcl_:1760628758367" -O "$PROJECT_ROOT/benchmarks.zip"
 tar -xzf "$PROJECT_ROOT/benchmarks.zip" -C "$PROJECT_ROOT"
 # rm "$PROJECT_ROOT/benchmarks.zip"
 
