b22e2f40eb6206de1ed1043c2638d9782c199983
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2026-02-04 14:39:47 +0100

    Measure optimization and execution time separately for DuckDB

--- src/main/scala/experiments/runner/DuckDBRunner.scala
+++ src/main/scala/experiments/runner/DuckDBRunner.scala
@@ -5,6 +5,12 @@ import experiments.readInOneLine
 
 import java.nio.file.{Files, Paths, StandardOpenOption}
 import experiments.getTimestamp
+import scala.sys.process._
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.collection.mutable
+import experiments.median
 
 object DuckDBRunner extends BaseRunner {
 	def main(args: Array[String]): Unit = {
@@ -15,7 +21,7 @@ object DuckDBRunner extends BaseRunner {
 
 			Files.write(
 				resultsPath,
-				"query,total_time\n".getBytes,
+				"query,opt_time,exec_time,total_time\n".getBytes,
 				StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING
 			)
 
@@ -28,11 +34,25 @@ object DuckDBRunner extends BaseRunner {
 
 				val query = readInOneLine(sqlFile)
 
-				val time = runQuery(query)
+				val command = Seq("./duckdb/build/release/duckdb", "-readonly", dbFilePath(benchmark), "-c", "EXPLAIN " + query)
 
-				println(s"Total time: $time us")
+				val optTime = (for (i <- 0 until repeatTimes) yield {
+					val output = mutable.ListBuffer[String]()
+					val process = Process(command).run(ProcessLogger(fout = output.addOne, ferr = output.addOne))
+					val future = Future(process.exitValue())
+					val exitCode = Await.result(future, timeout)
+					output.find(line => line.contains("Join order optimization time: ")).get.stripPrefix("Join order optimization time: ").stripSuffix(" us").toDouble
+				}).median()
 
-				Files.write(resultsPath, s"${sqlFile.getName.stripSuffix(".sql")},$time\n".getBytes, StandardOpenOption.APPEND)
+				println(s"Optimization time: $optTime us")
+
+				val totalTime = runQuery(query).toDouble
+				val execTime = if totalTime < timeout.toMicros - 1e-5 then totalTime - optTime else timeout.toMicros
+				println(s"Execution time: $execTime us")
+				println(s"Total time: $totalTime us")
+
+
+				Files.write(resultsPath, s"${sqlFile.getName.stripSuffix(".sql")},$optTime,$execTime,$totalTime\n".getBytes, StandardOpenOption.APPEND)
 			)
 
 			conn.close()
